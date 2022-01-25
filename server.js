fef63650d1dba0d5d389724171159419ffdb5d91
iMac7 <macharian77@gmail.com>
2022-01-25 07:29:33 +0300

    connected to server

--- server.js
+++ server.js
@@ -3,5 +3,17 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE'))
+    next()
+})
+
+
+app.get('/', (req, res) => console.log('get / successful'))
+app.post('/', (req, res) => console.log(req.body))
+
 app.listen(port, () => console.log(`listening on port ${port}!`))
